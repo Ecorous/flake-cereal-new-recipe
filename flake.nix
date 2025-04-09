6d233be788d3514306b8a57cfe09be007a19d338
Ecorous <ecorous@outlook.com>
2025-04-10 00:14:59 +0100

    add yet another missing semicolon

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
 
     input = {
         nixpkgs.url = "github:nixos/nixpkgs/nixpks?ref=nixos-unstable";
+        home-manager.url = "github:nix-community/home-manager";
+        home-manager.inputs.nixpkgs.follows = "nixpkgs";
     };
 
     outputs = inputs@{ self, nixpkgs, home-manager }: {
