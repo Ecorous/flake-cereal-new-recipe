7a886323145d5b7c806a18e3d358944eefce57ba
Ecorous <ecorous@outlook.com>
2025-04-10 21:54:56 +0100

    okay maybe things are fixed now?

--- common/home-manager.nix
+++ common/home-manager.nix
@@ -1,4 +1,13 @@
+{ pkgs, ... }:
 {
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
+  nixpkgs.overlays = [
+    (final: prev: {
+      openssh = prev.openssh.overrideAttrs (old: {
+        patches = (old.patches or [ ]) ++ [ ../files/openssh-nocheckcfg.patch ];
+        doCheck = false;
+      });
+    })
+  ];
 }
