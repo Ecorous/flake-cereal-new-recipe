7939262a5fa6e1d4163e14660b3ef360e36fdfc7
Ecorous <ecorous@outlook.com>
2025-04-10 01:43:24 +0100

    fix the orang (20)

--- juniper/hardware.nix
+++ juniper/hardware.nix
@@ -25,5 +25,5 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = 
-    lib.mkDefault config.hardware.allowRedistributableFirmware;
+    lib.mkDefault true;
 }
