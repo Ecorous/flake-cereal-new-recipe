689d8f7686d4ff87ce6f6864220d885ec5781a93
Ecorous <ecorous@outlook.com>
2025-04-13 18:06:24 +0100

    [elder] maybe done

--- elder/network.nix
+++ elder/network.nix
@@ -25,5 +25,80 @@
         interface = "eth";
       };
     };
+    nat.enable = false;
+    ruleset = ''
+      table inet filter {
+        # enable flow offloading for better throughput
+        flowtable f {
+          hook ingress priority 0;
+          devices = { wan, lan };
+        }
+
+        chain output {
+          type filter hook output priority 100; policy accept;
+        }
+
+        chain input {
+          type filter hook input priority filter; policy drop;
+
+          # Allow trusted networks to access the router
+          iifname {
+            "lan",
+          } counter accept
+
+          # Allow returning traffic from ppp0 and drop everthing else
+          iifname "wan" ct state { established, related } counter accept
+          iifname "wan" drop
+        }
+
+        chain forward {
+          type filter hook forward priority filter; policy drop;
+
+          # enable flow offloading for better throughput
+          ip protocol { tcp, udp } flow offload @f
+
+          # Allow trusted network WAN access
+          iifname {
+                  "lan",
+          } oifname {
+                  "wan",
+          } counter accept comment "Allow trusted LAN to WAN"
+
+          # Allow established WAN to return
+          iifname {
+                  "wan",
+          } oifname {
+                  "lan",
+          } ct state established,related counter accept comment "Allow established back to LANs"
+        }
+      }
+
+      table ip nat {
+        chain prerouting {
+          type nat hook prerouting priority filter; policy accept;
+        }
+
+        # Setup NAT masquerading on the ppp0 interface
+        chain postrouting {
+          type nat hook postrouting priority filter; policy accept;
+          oifname "wan" masquerade
+        }
+      }
+    '';
+  };
+  services.dhcpd4 = {
+    enable = true;
+    interfaces = [ "lan" ];
+    extraConfig = ''
+      option domain-name-servers 1.1.1.1 1.0.0.1;
+      option subnet-mask 255.255.255.0;
+
+      subnet 192.168.69.0 netmask 255.255.255.0 {
+        option broadcast-address 192.168.69.255;
+        option routers 192.168.69.1;
+        interface lan;
+        range 192.168.69.2 192.168.69.254;
+      }
+    ''
   };
 }
