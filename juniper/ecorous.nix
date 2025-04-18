927ec9128bf956f00d9007b8b99bd5f68ae04488
Ecorous <ecorous@outlook.com>
2025-04-19 00:14:22 +0100

    change brightness settings

--- juniper/ecorous.nix
+++ juniper/ecorous.nix
@@ -6,7 +6,11 @@
   ];
 
   home.file."scripts/brightness_down.sh".source = ../files/brightness_down.sh;
+  home.file."scripts/brightness_down.sh".executable = true;
   home.file."scripts/brightness_up.sh".source = ../files/brightness_up.sh;
+  home.file."scripts/brightness_up.sh".executable = true;
   home.file."scripts/brightness_set_1.sh".source = ../files/brightness_set_1.sh;
+  home.file."scripts/brightness_set_1.sh".executable = true;
   home.file."scripts/brightness_toggle.nu".source = ../files/brightness_toggle.nu;
+  home.file."scripts/brightness_toggle.nu".executable = true;
 }
