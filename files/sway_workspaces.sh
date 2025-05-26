28e9da1e78d3ca2e4ec9c458860b2b7206610f0f
Ecorous <ecorous@outlook.com>
2025-05-26 21:43:16 +0100

    meow meow

---
+++ files/sway_workspaces.sh
@@ -0,0 +1,7 @@
+sleep 30
+swaymsg rename workspace 1 to 1:"1: terminal"
+swaymsg rename workspace 2 to 2:"2: vesktop"
+swaymsg rename workspace 3 to 3:"3: firefox"
+swaymsg rename workspace 4 to 4:"4: signal"
+swaymsg rename workspace 5 to 5:"5: element"
+swaymsg rename workspace 10 to 10:"10: bluetooth"
