55d94b5fa91fabea9f80fd51c6203aac49337573
Ecorous <ecorous@outlook.com>
2025-06-30 19:25:48 +0100

    do the thing

--- common/plasma.nix
+++ common/plasma.nix
@@ -12,8 +12,4 @@
 
   services.desktopManager.plasma6.enable = true;
   # services.displayManager.defaultSession = lib.mkDefault "plasma";
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 0938a866a31de7f98db4a8b129b2e76057fae2a2
