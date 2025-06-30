c2ec6744e7882deb967ea04099638612931ff33a
Ecorous <ecorous@outlook.com>
2025-06-30 19:24:09 +0100

    do the thing

--- elder/hardware.nix
+++ elder/hardware.nix
@@ -8,7 +8,6 @@
 
 {
   imports = [
-    ../common/hardware.nix
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
--- yggdrasil/hardware.nix
+++ yggdrasil/hardware.nix
@@ -11,7 +11,6 @@
   # gpu: gtx 1080 (as of ~2023-10?)
 
   imports = [
-    ../common/hardware.nix
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
