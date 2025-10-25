330d4c07c17f2d6098ed92fc0d52a1a501091b26
Ecorous <ecorous@outlook.com>
2025-10-25 17:50:56 +0100

    fix nushellWith #3

--- common/nushellWith.nix
+++ common/nushellWith.nix
@@ -4,10 +4,7 @@
   nushellWith,
   inputs,
   ...
-}: let pkgs = import pkgs {
-    system = "x86_64-linux";
-    overlays = [ nushellWith.overlays.default ];
-  }; in {
+}: {
   environment.systemPackages = [
     pkgs.nushell
   ];
